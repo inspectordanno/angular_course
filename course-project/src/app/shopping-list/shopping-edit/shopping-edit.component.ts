3860c745426653f45627646b7cffd98a52ec00ab
inspectordanno <danno2423@gmail.com>
2019-12-20 09:29:26 -0500

    Course Project - Components & Databinding

--- course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: true }) nameInputRef;
+  @ViewChild('amountInput', { static: true }) amountInputRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  
+  ingredients = [];
+
+  addIngredient() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
+
+  deleteIngredient() {
+
+  }
+
+  clearIngredients() {
+
+  }
+
 
   constructor() { }
 
