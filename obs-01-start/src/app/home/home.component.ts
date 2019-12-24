5c8c525f0bec9bf838e6c0cabddfda0f42b3f345
inspectordanno <danno2423@gmail.com>
2019-12-24 13:38:17 -0500

    Observables & You!

---
+++ obs-01-start/src/app/home/home.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription, Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription: Subscription;
+
+  constructor() { }
+
+  ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count == 4) {
+          observer.complete();
+        }
+        if (count == 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
+        count++;
+      }, 1000)
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
+      console.log(data);
+    }, (error) => {
+      console.log(error);
+    }, () => {
+      console.log('completed!');
+    });
+  }
+
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
+  }
+
+}
