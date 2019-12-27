a8c4a19aa5dafa58ed006dedef502d31ad1318a3
inspectordanno <danno2423@gmail.com>
2019-12-27 18:21:00 -0500

    Error Handling & UX

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post-model';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
 
 
 @Injectable({
@@ -9,6 +10,7 @@ import { map } from 'rxjs/operators';
 })
 export class PostsService {
   firebaseUrl = 'https://ng-complete-guide-ffa0c.firebaseio.com/posts.json';
+  error = new Subject<string>();
 
   constructor(private http: HttpClient) { }
 
@@ -19,6 +21,8 @@ export class PostsService {
       .post<{name: string}>(this.firebaseUrl, postData)
       .subscribe(responseData => {
         console.log(responseData);
+      }, error => {
+        this.error.next(error.message);
       });
   }
 
