3c6363bf8467172593e0a21077f1c2bb256ea62e
inspectordanno <danno2423@gmail.com>
2020-01-02 15:54:49 -0500

    Fetching Recipe Detail Data

--- course-project/src/app/shared/data-storage.service.ts
+++ course-project/src/app/shared/data-storage.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, tap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import * as fromApp from '../store/app.reducer';
+import * as RecipesActions from '../recipes/store/recipe.actions';
+
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
   readonly firebaseURL = 'https://angular-course-project-226a9.firebaseio.com/recipes.json';
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService,
+    private store: Store<fromApp.AppState>) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -37,7 +42,8 @@ export class DataStorageService {
           });
         }),
         tap(recipes => {
-          this.recipeService.setRecipes(recipes);
+          //this.recipeService.setRecipes(recipes);
+          this.store.dispatch(new RecipesActions.SetRecipes(recipes));
         })
       )
   }
